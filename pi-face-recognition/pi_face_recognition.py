229d0b7248e85a4a1eb1ec82829c08c3df07aba0
IwanB038 <58119788+IwanB038@users.noreply.github.com>
2020-04-16 12:55:58 +0200

    Update pi_face_recognition.py

--- pi-face-recognition/pi_face_recognition.py
+++ pi-face-recognition/pi_face_recognition.py
@@ -1,7 +1,4 @@
-# USAGE
-# python pi_face_recognition.py --cascade haarcascade_frontalface_default.xml --encodings encodings.pickle
-
-# import the necessary packages
+# importeer de benodigde bestanden
 from imutils.video import VideoStream
 from imutils.video import FPS
 import face_recognition
@@ -11,7 +8,7 @@ import pickle
 import time
 import cv2
 
-# construct the argument parser and parse the arguments
+# creër de argumenten ontleder en ontleed de argumenten
 ap = argparse.ArgumentParser()
 ap.add_argument("-c", "--cascade", required=True,
     help = "path to where the face cascade resides")
@@ -19,102 +16,98 @@ ap.add_argument("-e", "--encodings", required=True,
     help="path to serialized db of facial encodings")
 args = vars(ap.parse_args())
 
-# load the known faces and embeddings along with OpenCV's Haar
-# cascade for face detection
-print("[INFO] loading encodings + face detector...")
+# laad de bekende gezichten en de gezichtsomvang samen met OpenCV's Haar
+# cascade voor de gezichts detectie
+print("[INFO] coderingen laden + gezichts detector...")
 data = pickle.loads(open(args["encodings"], "rb").read())
 detector = cv2.CascadeClassifier(args["cascade"])
 
-# initialize the video stream and allow the camera sensor to warm up
-print("[INFO] starting video stream...")
-# vs = VideoStream(src=0).start()
+# de video stream voorbereiden en de camerasensor laten opwarmen
+print("[INFO] de videostream starten...")
 vs = VideoStream(usePiCamera=True).start()
 time.sleep(2.0)
 
 # start de FPS teller
 fps = FPS().start()
 
-# loop over frames from the video file stream
+# loop over frames van de video stream
 while True:
-    # grab the frame from the threaded video stream and resize it
-    # to 500px (to speedup processing)
+    # pak het frame van de video stream en verklein het
+    # tot 500px (om het process te versnellen)
     frame = vs.read()
     frame = imutils.resize(frame, width=500)
     
-    # convert the input frame from (1) BGR to grayscale (for face
-    # detection) and (2) from BGR to RGB (for face recognition)
+    # zet het invoer frame om van (1) BGR naar grijswaarden (voor gezichts
+    # detectie) en (2) van BGR naar RGB (voor gezichtsherkenning)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    # detect faces in the grayscale frame
+    # detecteer gezichten in de grijswaarden
     rects = detector.detectMultiScale(gray, scaleFactor=1.1, 
         minNeighbors=5, minSize=(30, 30),
         flags=cv2.CASCADE_SCALE_IMAGE)
 
-    # OpenCV returns bounding box coordinates in (x, y, w, h) order
-    # but we need them in (top, right, bottom, left) order, so we
-    # need to do a bit of reordering
+    # OpenCV stuurt coordinaten van het gezichtskader in (x, y, w, h) volgorde terug
+    # maar ze moeten in (top, right, bottom, left) volgorde
     boxes = [(y, x + w, y + h, x) for (x, y, w, h) in rects]
 
-    # compute the facial embeddings for each face bounding box
+    # bereken de gezichtsomvang voor elk gezichtskader
     encodings = face_recognition.face_encodings(rgb, boxes)
     names = []
 
-    # loop over the facial embeddings
+    # loop over de gezichtskaders
     for encoding in encodings:
-        # attempt to match each face in the input image to our known
-        # encodings
+        # probeer elk gezicht in de invoer te matchen met een bekend gezicht
         matches = face_recognition.compare_faces(data["encodings"],
             encoding)
         name = "Unknown"
 
-        # check to see if we have found a match
+        # controleer of we een match hebben gevonden
         if True in matches:
-            # find the indexes of all matched faces then initialize a
-            # dictionary to count the total number of times each face
-            # was matched
+            # zoek de indexen van alle overeenkomende gezichten en houdt 
+            # van elk gezicht bij hoevaak ze overeen kwamen
             matchedIdxs = [i for (i, b) in enumerate(matches) if b]
             counts = {}
 
-            # loop over the matched indexes and maintain a count for
-            # each recognized face face
+            # loop over de overeenkomende indexen en houd een telling bij voor
+            # elk herkend gezichtsvlak
             for i in matchedIdxs:
                 name = data["names"][i]
                 counts[name] = counts.get(name, 0) + 1
 
-            # determine the recognized face with the largest number
-            # of votes (note: in the event of an unlikely tie Python
-            # will select first entry in the dictionary)
+            # bepaal het herkende gezicht met het hoogste getal
+            # (opmerking: als er twee gezichten zijn met een evenhoog getal 
+            # zal Python het eerste gezicht selecteren)
             name = max(counts, key=counts.get)
         
-        # update the list of names
+        # update de lijst met namen
         names.append(name)
 
-    # loop over the recognized faces
+    # loop over de herkende gezichten
     for ((top, right, bottom, left), name) in zip(boxes, names):
-        # draw the predicted face name on the image
+        # zet de voorspelde gezichtsnaam op het gezicht
         cv2.rectangle(frame, (left, top), (right, bottom),
             (0, 255, 0), 2)
         y = top - 15 if top - 15 > 15 else top + 15
         cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
             0.75, (0, 255, 0), 2)
 
-    # display the image to our screen
+    # toon de het frame op ons scherm 
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1) & 0xFF
 
-    # if the `q` key was pressed, break from the loop
+    # als de 'q' toets ingedrukt wordt, stopt de loop
     if key == ord("q"):
         break
 
-    # update the FPS counter
+    # update de FPS teller
     fps.update()
 
-# stop the timer and display FPS information
+# stop de timer en geef de FPS informatie weer
 fps.stop()
-print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
-print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+print("[INFO] verlopen tijd: {:.2f}".format(fps.elapsed()))
+print("[INFO] ca. FPS: {:.2f}".format(fps.fps()))
 
-# do a bit of cleanup
+# om alles schoon te houden
 cv2.destroyAllWindows()
 vs.stop()

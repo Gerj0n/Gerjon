e22d100bf7d85de6ceed739c84fa8c1532bcd934
IwanB038 <58119788+IwanB038@users.noreply.github.com>
2020-04-16 13:08:13 +0200

    Update encode_faces.py

--- pi-face-recognition/encode_faces.py
+++ pi-face-recognition/encode_faces.py
@@ -1,10 +1,4 @@
-# USAGE
-# When encoding on laptop, desktop, or GPU (slower, more accurate):
-# python encode_faces.py --dataset dataset --encodings encodings.pickle --detection-method cnn
-# When encoding on Raspberry Pi (faster, more accurate):
-# python encode_faces.py --dataset dataset --encodings encodings.pickle --detection-method hog
-
-# import the necessary packages
+# importeer de benodigde bestanden
 from imutils import paths
 import face_recognition
 import argparse
@@ -12,7 +6,7 @@ import pickle
 import cv2
 import os
 
-# construct the argument parser and parse the arguments
+# creër de opdracht ontleder en ontleed de opdrachten
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--dataset", required=True,
     help="path to input directory of faces + images")
@@ -22,42 +16,42 @@ ap.add_argument("-d", "--detection-method", type=str, default="cnn",
     help="face detection model to use: either `hog` or `cnn`")
 args = vars(ap.parse_args())
 
-# grab the paths to the input images in our dataset
-print("[INFO] quantifying faces...")
+# pak de paden naar de invoer afbeeldingen in onze database
+print("[INFO] gezichten instellen...")
 imagePaths = list(paths.list_images(args["dataset"]))
 
-# initialize the list of known encodings and known names
+# initialiseer de lijst met bekende coderingen en bekende namen
 knownEncodings = []
 knownNames = []
 
-# loop over the image paths
+# loop over de afbeelding paden
 for (i, imagePath) in enumerate(imagePaths):
-    # extract the person name from the image path
-    print("[INFO] processing image {}/{}".format(i + 1,
+    # haal de naam van de persoon uit het afbeeldingspad
+    print("[INFO] afbeelding verwerken {}/{}".format(i + 1,
         len(imagePaths)))
     name = imagePath.split(os.path.sep)[-2]
 
-    # load the input image and convert it from RGB (OpenCV ordering)
-    # to dlib ordering (RGB)
+    # laad de invoer afbeelding en zet hem om van RGB (OpenCV ordening)
+    # naar dlib ordening (RGB)
     image = cv2.imread(imagePath)
     rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-    # detect the (x, y)-coordinates of the bounding boxes
-    # corresponding to each face in the input image
+    # detecteer de (x, y)-coordinaten van het gezichtskader
+    # overeenkomend met elk gezicht in de invoer afbeelding
     boxes = face_recognition.face_locations(rgb,
         model=args["detection_method"])
 
-    # compute the facial embedding for the face
+    # bereken het kader voor het gezicht
     encodings = face_recognition.face_encodings(rgb, boxes)
 
-    # loop over the encodings
+    # loop over de coderingen
     for encoding in encodings:
-        # add each encoding + name to our set of known names and
-        # encodings
+        # voeg elke codering + naam toe aan onze reeks van bekende namen
+        # coderingen
         knownEncodings.append(encoding)
         knownNames.append(name)
 
-# dump the facial encodings + names to disk
+# dump de gezichts coderingen + namen naar het bestand
 print("[INFO] serializing encodings...")
 data = {"encodings": knownEncodings, "names": knownNames}
 f = open(args["encodings"], "wb")
